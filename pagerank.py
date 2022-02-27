cc4c9da5deda876bd3fcdeccc4227f6ba3a97ed9
Bob Liu <527080292@qq.com>
2022-02-28 02:56:33 +0800

    Improve the pagerank function

--- pagerank.py
+++ pagerank.py
@@ -1,6 +1,7 @@
 from numpy import *
-a = array([[0,1,1,0],[1,0,0,1],[1,0,0,1],[1,1,0,0]],dtype=float)
-#构造转移矩阵
+#
+#构造转移矩阵a = array([[0,0,0,1],[0.5,0,0,0],[1/2,1,0,0],[0,0,1,0]],dtype=float)
+a = array([[1/2,1/2,0],[1/2,0,1],[0,1/2,0]],dtype=float)
 def transPre(data):
     b = transpose(data) #把矩阵转置
     c = zeros((a.shape),dtype=float)
@@ -17,19 +18,30 @@ def initiPre(c):
     pr = zeros((c.shape[0],1),dtype=float)
     for i in range(c.shape[0]):
         pr[i] = float(1)/c.shape[0]
+    print("this is pr:",pr)
     return pr
 # print(initiPre(a))
 
 def PageRank(p,m,v):
     #pageRank算法
     #p是网页跳转概率，m是转移矩阵，v是pr值
+    i=0
     while ((v == p*dot(m,v) + (1-p)*v).all() == False):
         v = p*dot(m,v) + (1-p)*v
         # print(v)
-        print((v == p*dot(m,v) + (1-p)*v).all())
+        #print((v == p*dot(m,v) + (1-p)*v).all())
+
+        if i<=20:#迭代次数
+            print("第%d次:" % (i + 1), v)
+
+        elif i==100:
+            break
+        i += 1
+
+
     return v
 if __name__ == '__main__':
     M = transPre(a)
     pr = initiPre(M)
-    p = 0.85
+    p = 1#β的值
     print(PageRank(p,M,pr))
