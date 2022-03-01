e4fd5eb8788291233391eaa2bbf04987a8e6d0b8
Bob Liu <527080292@qq.com>
2022-03-01 20:02:21 +0800

    kmeans cluster calculate

--- Clustering.py
+++ Clustering.py
@@ -3,37 +3,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn import preprocessing
-#question 1
-data=pd.read_csv(r"clustering_data.csv",delimiter=",",header=0,index_col=0)
-#scaler=preprocessing.StandardScaler().fit(data)
-d=data.values.tolist()
-X=np.array(d)
-kmeans = KMeans(init="random").fit(X)
-print(kmeans.labels_)
-SSE=kmeans.inertia_
-print(SSE)
+
+X=[[1,2],[2,1],[3,5],[5,3],[6,1]]
+
 
 #question 2
-kmeans = KMeans(init="k-means++",n_clusters=8,n_init=100,max_iter=10000,random_state=10).fit(X)
+kmeans = KMeans(init="k-means++",n_clusters=2,n_init=100,max_iter=10000).fit(X)
 print(kmeans.labels_)
 SSE=kmeans.inertia_
 print(SSE)
 
-#question 3
-name=data.index
-clu=[]
-i=0
-lab=kmeans.labels_
-lab=lab.tolist()
-dict={}
-for each in lab:
-    if each not in dict.keys():
-        dict[each]=[name[i]]
-        #print(dict)
-    else:
-        temp=dict[each]
-        temp.append(name[i])
-        dict[each] = temp
-    i+=1
-print(dict)
+
 
